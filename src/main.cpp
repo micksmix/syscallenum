002a0766208b5b540b92ee1dc354cde92c0d2cd0
Mick Grove <mgrove@gmail.com>
2018-12-31 16:20:37 -0800

    working without pipes. No json output support

--- src/main.cpp
+++ src/main.cpp
@@ -1,118 +1,29 @@
-#include <cstring>
-#include <iostream>
+#include <errno.h>
 #include <seccomp.h>
-#include <sstream>
-#include <sys/syscall.h>
-#include <sys/wait.h>
+#include <iostream>
+#include <sys/types.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/syscall.h>
+#include <sstream>
+#include <iomanip>
 #include <vector>
 
 using namespace std;
 
-// global variables
-const int BUFFER_SIZE = 1024;
-const int READ_END = 0;
-const int WRITE_END = 1;
 const int NUM_SYSCALLS = 330;
 
-/*  Convenience function to make a pair of pipes  */
-void make_pipe_pair(int *pair1, int *pair2) {
-  if (pipe(pair1) == -1 || pipe(pair2) == -1) {
-    cerr << "couldn't create pipe";
-    exit(EXIT_FAILURE);
-  }
-}
+void printJsonOutput(string &sReadMsg, bool &bFirstResult)
+{
 
-/*  Convenience function to close a pair of file descriptors  */
-void close_pair(const int rfd, const int wfd) {
-  if (close(rfd) == -1 || close(wfd) == -1) {
-    cerr << "couldn't close file";
-    exit(EXIT_FAILURE);
-  }
-}
-
-/*  Main child process function  */
-void child_func(const int wpipe, const auto idx) {
-  // child
-  long long int ret = -1;
-
-  auto arch = seccomp_arch_native();
-  auto *name = seccomp_syscall_resolve_num_arch(arch, idx);
-
-  // these cause a hang, so just skip
-  if (idx == SYS_pause) {
-    exit(0);
-  }
-  if (idx == SYS_rt_sigreturn) {
-    exit(0);
-  }
-  if (idx == SYS_select) {
-    exit(0);
-  }
-  if (idx == SYS_pselect6) {
-    exit(0);
-  }
-  if (idx == SYS_ppoll) {
-    exit(0);
-  }
-
-  // exit_group and exit -- causes us to exit
-  if (idx == SYS_exit) {
-    exit(0);
-  }
-  if (idx == SYS_exit_group) {
-    exit(0);
-  }
-
-  // things currently break horribly if  CLONE, FORK or VFORK are called and the
-  // call succeeds guess it should be straight forward to kill the forks
-  // if (idx == SYS_clone) { exit(0); }
-  // if (idx == SYS_fork) { exit(0); }
-  // if (idx == SYS_vfork) { exit(0); }
-  ret = syscall(idx, 0, 0, 0);
-
-  // check both EPERM and EACCES - LXC returns EACCES and Docker EPERM
-  std::string sStatus;
-  if ((ret == EPERM) || (ret == EACCES)) {
-    // cerr << "blocked" << endl;
-    sStatus = "filtered";
-  } else {
-    // cerr << "allowed" << endl;
-    sStatus = "allowed";
-  }
-
-  char writeMsg[BUFFER_SIZE];
-  sprintf(writeMsg, "%u|%s|%s|%s|%d", idx, name, sStatus.c_str(),
-          strerror(errno), errno);
-  if (write(wpipe, writeMsg, strlen(writeMsg) + 1) == -1) {
-    exit(EXIT_FAILURE);
-  }
-
-  /*  Close file descriptors and exit  */
-  close(wpipe);
-  exit(0);
-}
-
-int Usage(char *ProgramName) {
-  cout << endl;
-  cout << "Usage: " << ProgramName << " [-f] [-a] [-j]" << endl
-       << "    or " << ProgramName << endl;
-  cout << "  -f     Show only filtered syscalls" << endl;
-  cout << "  -a     Show only allowed syscalls" << endl;
-  cout << "  -j     Output as json" << endl;
-  cout << endl;
-  cout << "NOTE: By default, both allowed and filtered syscalls are returned"
-       << endl;
-  return -1;
-}
-
-void printJsonOutput(string &sReadMsg, bool &bFirstResult) {
-	
   size_t last = 0;
   size_t next = 0;
   std::vector<string> vResults;
 
-  while ((next = sReadMsg.find("|", last)) != string::npos) {
+  while ((next = sReadMsg.find("|", last)) != string::npos)
+  {
     // cout << sReadMsg.substr(last, next - last) << endl;
     vResults.push_back(sReadMsg.substr(last, next - last));
     last = next + 1;
@@ -121,14 +32,20 @@ void printJsonOutput(string &sReadMsg, bool &bFirstResult) {
   // cout << sReadMsg.substr(last) << endl;
 
   // idx, name, sStatus.c_str(), strerror(errno), errno);
-  if (bFirstResult) {
+  if (bFirstResult)
+  {
     cout << "  {" << endl;
     bFirstResult = false;
-  } else {
-    cout << "," << endl << "  {" << endl;
   }
-  for (auto idx = 0; idx < vResults.size(); idx++) {
-    switch (idx) {
+  else
+  {
+    cout << "," << endl
+         << "  {" << endl;
+  }
+  for (auto idx = 0; idx < vResults.size(); idx++)
+  {
+    switch (idx)
+    {
     case 0:
       cout << "    "
            << "\"number\":"
@@ -161,26 +78,45 @@ void printJsonOutput(string &sReadMsg, bool &bFirstResult) {
   cout << "  }";
 }
 
-int main(int argc, char *argv[]) {
+int Usage(char *ProgramName)
+{
+  cout << endl;
+  cout << "Usage: " << ProgramName << " [-f] [-a]" << endl
+       << "    or " << ProgramName << endl;
+  cout << "  -f     Show only filtered syscalls" << endl;
+  cout << "  -a     Show only allowed syscalls" << endl;
+  cout << endl;
+  cout << "NOTE: By default, both allowed and filtered syscalls are returned"
+       << endl;
+  return -1;
+}
+
+int main(int argc, char *argv[])
+{
+  unsigned int pid, idx;
+  int ret;
 
   int arg;
   bool bFiltered = false;
   bool bAllowed = false;
   bool bUnknownParam = false;
-  bool bJsonOutput = false;
-  bool bFirstResult = true;
 
-  if (argc > 3) {
+  if (argc > 3)
+  {
     return Usage(argv[0]);
   }
 
-  if (argc > 1) {
-    for (arg = 1; arg < argc; arg++) {
-      switch (argv[arg][0]) {
+  if (argc > 1)
+  {
+    for (arg = 1; arg < argc; arg++)
+    {
+      switch (argv[arg][0])
+      {
 
       case L'-':
       case L'/':
-        switch (argv[arg][1]) {
+        switch (argv[arg][1])
+        {
         case L'?':
           return Usage(argv[0]);
         case L'h':
@@ -193,10 +129,6 @@ int main(int argc, char *argv[]) {
         case L'A':
           bAllowed = true;
           break;
-        case L'j': // json output
-        case L'J':
-          bJsonOutput = true;
-          break;
         default:
           bUnknownParam = true;
           break;
@@ -206,95 +138,111 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  if (bUnknownParam) {
+  if (bUnknownParam)
+  {
     return Usage(argv[0]);
   }
 
-  if ((!bFiltered) && (!bAllowed)) {
+  if ((!bFiltered) && (!bAllowed))
+  {
     // means no params passed. Assume default of printing both
     bFiltered = true;
     bAllowed = true;
   }
 
-  int ptoc_fd[NUM_SYSCALLS][2]; /*  Parent to child pipes    */
-  int ctop_fd[NUM_SYSCALLS][2]; /*  Child to parent pipes    */
-  pid_t children[NUM_SYSCALLS]; /*  Process IDs of children  */
+  for (idx = 0; idx <= NUM_SYSCALLS; idx++)
+  {
+    pid = fork();
+    if (!pid)
+    { // child
+      auto arch = seccomp_arch_native();
+      auto *name = seccomp_syscall_resolve_num_arch(arch, idx);
 
-  char readMsg[BUFFER_SIZE];
+      bool bCallFiltered = false;
+      bool bCallAllowed = false;
 
-  /*  Create pipe pairs and fork children  */
-  for (auto i = 0; i < NUM_SYSCALLS; ++i) {
-    make_pipe_pair(ptoc_fd[i], ctop_fd[i]);
+      // these cause a hang, so just skip
+      if (idx == SYS_pause) {
+        exit(0);
+      }
+      if (idx == SYS_rt_sigreturn) {
+        exit(0);
+      }
+      if (idx == SYS_select) {
+        exit(0);
+      }
+      if (idx == SYS_pselect6) {
+        exit(0);
+      }
+      if (idx == SYS_ppoll) {
+        exit(0);
+      }
 
-    if ((children[i] = fork()) == -1) {
-      cerr << "error calling fork()";
-      return EXIT_FAILURE;
-    } else if (children[i] == 0) {
-      close_pair(ctop_fd[i][0], ptoc_fd[i][1]);
-      child_func(ctop_fd[i][1], i + 1);
-      return EXIT_SUCCESS;
-    } else {
-      close_pair(ptoc_fd[i][0], ctop_fd[i][1]);
-    }
-  }
+      // exit_group and exit -- causes us to exit
+      if (idx == SYS_exit) {
+        exit(0);
+      }
+      if (idx == SYS_exit_group) {
+        exit(0);
+      }
 
-  /*  Loop through each child  */
-  if (bJsonOutput){
-	  cout << "[" << endl;
-  }
+      // things currently break horribly if  CLONE, FORK or VFORK are called and the call succeeds
+      // guess it should be straight forward to kill the forks
+      //if (idx == SYS_clone) {exit(0);}
+      //if (idx == SYS_fork) {exit(0);}
+      //if (idx == SYS_vfork) {exit(0);}
+      ret = syscall(idx, 0, 0, 0);
 
-  for (auto i = 0; i < NUM_SYSCALLS; ++i) {
-    ssize_t num_read;
-    if ((num_read = read(ctop_fd[i][0], readMsg, sizeof(readMsg))) == -1) {
-      return EXIT_FAILURE;
-    } else if (num_read == 0) {
-      // cout << "Pipe from child " << i +1 << " closed." << endl;
-    } else {
+      // check both EPERM and EACCES - LXC returns EACCES and Docker EPERM
+      std::string sStatus;
+      if ((ret == EPERM) || (ret == EACCES))
+      {
+        // cerr << "blocked" << endl;
+        sStatus = "filtered";
+        bCallFiltered = true;
+      }
+      else
+      {
+        // cerr << "allowed" << endl;
+        sStatus = "allowed";
+        bCallAllowed = true;
+      }
 
-      std::string sReadMsg(readMsg);
+      std::ostringstream ssMessage;
+      ssMessage << std::setw(3) << std::setfill('0') << idx << "|"
+                << name << "|"
+                << sStatus << "|"
+                << strerror(errno) << "|"
+                << errno;
+      std::string sReadMsg = ssMessage.str();
+      //printf(" -- syscall(%u) is %s = %d : %s (%d)\n", idx, name, ret, strerror(errno), errno);
 
-      if ((bFiltered) && (bAllowed)) {
-        // actually prints *every* syscall, even unknown syscalls (e.g if you pass 475 as syscall, will print results)
-        if (bJsonOutput) {
-          printJsonOutput(sReadMsg, bFirstResult);
-        } else {
-          cout << sReadMsg << endl;
-        }
-      } else {
+      if ((bFiltered) && (bAllowed))
+      {
+        cout << sReadMsg << endl;
+      }
+      else
+      {
         if (bFiltered) {
-          if (sReadMsg.find("|filtered|") != std::string::npos) {
-            //cout << sReadMsg << endl;
-            if (bJsonOutput) {
-              printJsonOutput(sReadMsg, bFirstResult);
-            } else {
-              cout << sReadMsg << endl;
-            }
+          //if (sReadMsg.find("|filtered|") != std::string::npos) {
+          if (bCallFiltered) {
+            cout << sReadMsg << endl;
           }
         }
         if (bAllowed) {
-          if (sReadMsg.find("|allowed|") != std::string::npos) {
-            //cout << sReadMsg << endl;
-            if (bJsonOutput) {
-              printJsonOutput(sReadMsg, bFirstResult);
-            } else {
-              cout << sReadMsg << endl;
-            }
+          //if (sReadMsg.find("|allowed|") != std::string::npos) {
+          if (bCallAllowed) {
+            cout << sReadMsg << endl;
           }
         }
       }
-    }
-  }
 
-  if (bJsonOutput){
-	  cout << endl << "]" << endl;
-  }
-
-  /*  Clean up and harvest dead children  */
-  for (auto i = 0; i < NUM_SYSCALLS; ++i) {
-    if (waitpid(children[i], NULL, 0) == -1) {
-      return EXIT_FAILURE;
+      exit(0);
     }
-    close_pair(ptoc_fd[i][1], ctop_fd[i][0]);
+    else
+    {
+      usleep(100);
+    }
   }
 
   return 0;
